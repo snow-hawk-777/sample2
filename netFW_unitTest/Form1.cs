6d6ebd4020cc29f1ade70b80f39cf5eacac8d910
snow-hawk-777 <3x7x37eq777@gmail.com>
2025-04-09 17:19:10 +0900

    Add files via upload
    aaa

---
+++ netFW_unitTest/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace netFW_unitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
